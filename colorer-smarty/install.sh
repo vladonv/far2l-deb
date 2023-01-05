ff347f3ab95069bb000221a1276c28c164c41370
Ivan Sorokin <unxed@mail.ru>
2023-01-05 13:39:30 +0300

    Remove unneeded stuff; use themes/default_dark.tgz

--- colorer-smarty/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp -rf *.hrc ~/.config/far2l/plugins/colorer
-sed -i 's/UserHrcPath\=$/UserHrcPath\=\~\/\.config\/far2l\/plugins\/colorer\/user\.hrc/g' ~/.config/far2l/plugins/colorer/config.ini
-
